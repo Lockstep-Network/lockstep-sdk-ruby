9b70b2b07504c1b06af209a41fc350183143d9d3
ManishNarayan <manish.n@lockstep.io>
2022-01-20 13:14:13 +0530

    Adding User Accounts model.

--- lib/lockstep_sdk/clients/user_accounts.rb
+++ lib/lockstep_sdk/clients/user_accounts.rb
@@ -15,6 +15,8 @@
 
 class UserAccounts
 
+    require 'awrence'
+
     def initialize(lockstepsdk) # Initialize the Activities class with a lockstepsdk instance.
         @lockstepsdk = lockstepsdk
     end
@@ -25,7 +27,7 @@ class UserAccounts
     #  
     #  @param id [uuid] The unique ID number of the User to retrieve
     #  @param include [string] To fetch additional data on this object, specify the list of elements to retrieve. Available collections: Notes, Attachments, CustomFields, AccountingRole
-    def retrieve_user(include_param)
+    def retrieve_user(include_param=nil, id:)
         path = "/api/v1/UserAccounts/#{id}"
         params = {:include => include_param}
         @lockstepsdk.request(:get, path, nil, params)
@@ -37,9 +39,9 @@ class UserAccounts
     #  
     #  @param id [uuid] The unique ID number of the User to retrieve
     #  @param body [object] A list of changes to apply to this User
-    def update_user()
+    def update_user(body:, id:)
         path = "/api/v1/UserAccounts/#{id}"
-        @lockstepsdk.request(:patch, path, body, nil)
+        @lockstepsdk.request(:patch, path, body.to_camelback_keys.to_json, nil)
     end
 
     #  Disable the user referred to by this unique identifier.
@@ -47,7 +49,7 @@ class UserAccounts
     #  A User represents a person who has the ability to authenticate against the Lockstep Platform and use services such as Lockstep Insights.  A User is uniquely identified by an Azure identity, and each user must have an email address defined within their account.  All Users must validate their email to make use of Lockstep platform services.  Users may have different privileges and access control rights within the Lockstep Platform.
     #  
     #  @param id [uuid] The unique Lockstep Platform ID number of this User
-    def disable_user()
+    def disable_user(id:)
         path = "/api/v1/UserAccounts/#{id}"
         @lockstepsdk.request(:delete, path, nil, nil)
     end
@@ -57,7 +59,7 @@ class UserAccounts
     #  A User represents a person who has the ability to authenticate against the Lockstep Platform and use services such as Lockstep Insights.  A User is uniquely identified by an Azure identity, and each user must have an email address defined within their account.  All Users must validate their email to make use of Lockstep platform services.  Users may have different privileges and access control rights within the Lockstep Platform.
     #  
     #  @param id [uuid] The unique Lockstep Platform ID number of this User
-    def reenable_user(id)
+    def reenable_user(id:)
         path = "/api/v1/UserAccounts/reenable"
         params = {:id => id}
         @lockstepsdk.request(:post, path, nil, params)
@@ -68,16 +70,16 @@ class UserAccounts
     #  A User represents a person who has the ability to authenticate against the Lockstep Platform and use services such as Lockstep Insights.  A User is uniquely identified by an Azure identity, and each user must have an email address defined within their account.  All Users must validate their email to make use of Lockstep platform services.  Users may have different privileges and access control rights within the Lockstep Platform.
     #  
     #  @param body [InviteSubmitModel] The user to invite
-    def invite_user()
+    def invite_user(body:)
         path = "/api/v1/UserAccounts/invite"
-        @lockstepsdk.request(:post, path, body, nil)
+        @lockstepsdk.request(:post, path, body.to_camelback_keys.to_json, nil)
     end
 
     #  Retrieves invite information for the specified invite token.
     #  
     #  A User represents a person who has the ability to authenticate against the Lockstep Platform and use services such as Lockstep Insights.  A User is uniquely identified by an Azure identity, and each user must have an email address defined within their account.  All Users must validate their email to make use of Lockstep platform services.  Users may have different privileges and access control rights within the Lockstep Platform.
     #  @param code [uuid] The code of the invite
-    def retrieve_invite_data(code)
+    def retrieve_invite_data(code:)
         path = "/api/v1/UserAccounts/invite"
         params = {:code => code}
         @lockstepsdk.request(:get, path, nil, params)
@@ -88,9 +90,9 @@ class UserAccounts
     #  A User represents a person who has the ability to authenticate against the Lockstep Platform and use services such as Lockstep Insights.  A User is uniquely identified by an Azure identity, and each user must have an email address defined within their account.  All Users must validate their email to make use of Lockstep platform services.  Users may have different privileges and access control rights within the Lockstep Platform.
     #  
     #  @param body [TransferOwnerSubmitModel] 
-    def transfer_owner()
+    def transfer_owner(body:)
         path = "/api/v1/UserAccounts/transfer-owner"
-        @lockstepsdk.request(:post, path, body, nil)
+        @lockstepsdk.request(:post, path, body.to_camelback_keys.to_json, nil)
     end
 
     #  Queries Users for this account using the specified filtering, sorting, nested fetch, and pagination rules requested. A User represents a person who has the ability to authenticate against the Lockstep Platform and use services such as Lockstep Insights.  A User is uniquely identified by an Azure identity, and each user must have an email address defined within their account.  All Users must validate their email to make use of Lockstep platform services.  Users may have different privileges and access control rights within the Lockstep Platform.
@@ -100,7 +102,7 @@ class UserAccounts
     #  @param order [string] The sort order for this query. See See [Searchlight Query Language](https://developer.lockstep.io/docs/querying-with-searchlight)
     #  @param pageSize [int32] The page size for results (default 200). See [Searchlight Query Language](https://developer.lockstep.io/docs/querying-with-searchlight)
     #  @param pageNumber [int32] The page number for results (default 0). See [Searchlight Query Language](https://developer.lockstep.io/docs/querying-with-searchlight)
-    def query_users(filter, include_param, order, pageSize, pageNumber)
+    def query_users(pageSize = 200, pageNumber = 0, filter:, include_param:, order:)
         path = "/api/v1/UserAccounts/query"
         params = {:filter => filter, :include => include_param, :order => order, :pageSize => pageSize, :pageNumber => pageNumber}
         @lockstepsdk.request(:get, path, nil, params)
